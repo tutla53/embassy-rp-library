8094f0a27f25eb32fa76e1c2e5b79ec592dc3bb5
tutla <ayatullah_tutla@yahoo.com>
2025-01-15 15:20:45 +0700

    Fix the Frequency Selection Method

--- rp2040-servo/src/lib.rs
+++ rp2040-servo/src/lib.rs
@@ -9,6 +9,7 @@ use {
         SetDutyCycle
     },
     embassy_time::Timer,
+    fixed::{types::extra::U4, FixedU16},
     {defmt_rtt as _, panic_probe as _},
 };
 
@@ -16,7 +17,7 @@ const DEFAULT_SERVO_FREQ: u32 = 50; //Hertz
 const DEFAULT_MIN_PULSE_WIDTH: u32 = 1000; //us 
 const DEFAULT_MAX_PULSE_WIDTH: u32 = 2000;  //us
 const DEFAULT_MAX_DEGREE_ROTATION: u8 = 180; //degree
-const DEFAULT_INITIAL_POSITION: u32 = 0; //degree
+const DEFAULT_INITIAL_POSITION: u16 = 0; //degree
 
 pub struct ServoBuilder<'d> {
     pwm: Pwm<'d>,
@@ -25,7 +26,7 @@ pub struct ServoBuilder<'d> {
     min_pulse_width: u32,
     max_pulse_width: u32,
     max_degree_rotation: u8,
-    initial_position: u32,
+    initial_position: u16,
 }
 
 impl<'d> ServoBuilder<'d> {
@@ -61,25 +62,29 @@ impl<'d> ServoBuilder<'d> {
         self
     }
 
-    pub fn set_initial_position(mut self, init_pos: u32) -> Self {
+    pub fn set_initial_position(mut self, init_pos: u16) -> Self {
         self.initial_position = init_pos;
         self
     }
 
     pub fn build(mut self) -> Servo<'d> {
+        let clock_freq_hz = embassy_rp::clocks::clk_sys_freq();
+        let divider = 125u8;
+        let period = (clock_freq_hz / (self.freq * divider as u32)) as u16 - 1;
+        let num = FixedU16::<U4>::from_num(self.max_pulse_width - self.min_pulse_width);
         
-        let clock_freq = embassy_rp::clocks::clk_sys_freq();
-        self.cfg.top = (clock_freq / self.freq) as u16 - 1;
+        self.cfg.top = period;
+        self.cfg.divider = divider.into();
         self.pwm.set_config(&self.cfg);
 
         Servo {
             pwm: self.pwm,
             cfg: self.cfg,
-            period: 1_000_000/self.freq,
-            min_pulse_width: self.min_pulse_width,
-            max_pulse_width: self.max_pulse_width,
-            max_degree_rotation: self.max_degree_rotation,
+            min_pulse_width: FixedU16::<U4>::from_num(self.min_pulse_width),
+            max_pulse_width: FixedU16::<U4>::from_num(self.max_pulse_width),
+            max_degree_rotation:  FixedU16::<U4>::from_num(self.max_degree_rotation),
             current_pos: self.initial_position,
+            numerator: num,
         }
     }
 }
@@ -87,21 +92,21 @@ impl<'d> ServoBuilder<'d> {
 pub struct Servo<'d> {
     pwm: Pwm<'d>,
     cfg: Config,
-    period: u32,
-    min_pulse_width: u32,
-    max_pulse_width: u32,
-    max_degree_rotation: u8,
-    current_pos: u32,
+    min_pulse_width: FixedU16<U4>,
+    max_pulse_width: FixedU16<U4>,
+    max_degree_rotation: FixedU16<U4>,
+    current_pos: u16,
+    numerator: FixedU16<U4>,
 }
 
 #[allow(dead_code)]
 impl<'d> Servo<'d> {
-    fn set_current_pos(&mut self, degree: u32){
-        self.current_pos = degree
+    fn set_current_pos(&mut self, degree: u16){
+        self.current_pos = degree;
     }
 
-    pub fn get_current_pos(&mut self) -> u32 {
-        return self.current_pos
+    pub fn get_current_pos(&mut self) -> u16 {
+        return self.current_pos;
     }
 
     pub fn enable(&mut self) {
@@ -114,17 +119,21 @@ impl<'d> Servo<'d> {
         self.pwm.set_config(&self.cfg);
     }
 
-    pub fn rotate(&mut self, degree: u32) {
-        let micro_second_per_degree = (self.max_pulse_width - self.min_pulse_width)/(self.max_degree_rotation as u32);
-        let mut duration = degree * micro_second_per_degree + self.min_pulse_width;
+    pub fn rotate(&mut self, degree: u16) {
+
+        let mut duration = ((FixedU16::<U4>::from_num(degree)/self.max_degree_rotation) * self.numerator) + self.min_pulse_width;
         
-        if self.max_pulse_width < duration {duration = self.max_pulse_width;}
+        if  duration > self.max_pulse_width {
+            duration = self.max_pulse_width;
+        }
         
+        let duration_int = duration.to_num::<u16>();
+
         self.set_current_pos(degree);
-        self.pwm.set_duty_cycle_fraction(duration as u16, self.period as u16).unwrap();
+        self.pwm.set_duty_cycle(duration_int).unwrap();
     }
 
-    pub async fn sweep(&mut self, target: u32, delay_ms: u64){
+    pub async fn sweep(&mut self, target: u16, delay_ms: u64){
         let mut inc: i16 = 1;
         if self.current_pos > target {inc = -1}
     
@@ -134,8 +143,13 @@ impl<'d> Servo<'d> {
             if new_pos<0 {new_pos = 0;}
             else if new_pos>180{new_pos = 180;}
     
-            self.rotate(new_pos as u32);
+            self.rotate(new_pos as u16);
             Timer::after_millis(delay_ms).await;
         }
+    }
+
+    fn rotate_duration(&mut self, duration: u16) {
+        self.set_current_pos(duration.into());
+        self.pwm.set_duty_cycle(duration).unwrap();
     }
 }
